bdd1493b677f01c42fa742dd82f1c0a7cecb5d55
appeonwavish <tao.wu@appeon.com>
2017-08-31 19:02:05 +0800

    Test_revert

--- fdasf/fdasf.cpp
+++ fdasf/fdasf.cpp
@@ -2,11 +2,19 @@
 //
 
 #include "stdafx.h"
+#include <stdio.h>
 
+void Test_Revert()
+{
+	printf("Test_revert");
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	printf("hello world!");
+	Test_Revert();
 	return 0;
 }
+
+
 
